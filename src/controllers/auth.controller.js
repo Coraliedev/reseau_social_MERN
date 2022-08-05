3e11caa8dfbd9521bb96b9f62b12959c1cdff7d5
Coraliedev <coralie.acquaert@gmail.com>
2022-08-05 18:23:32 +0200

    feat(auth): add signInErrors

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const UserModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
-const { signUpErrors } = require("../utils/erros.utils");
+const { signUpErrors, signInErrors } = require("../utils/erros.utils");
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 const createToken = (id) => {
@@ -28,7 +28,8 @@ module.exports.signIn = async (req, res) => {
     res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge });
     res.status(200).json({ user: user._id });
   } catch (err) {
-    res.status(200).json(err);
+    const errors = signInErrors(err);
+    res.status(200).send({errors});
   }
 };
 
