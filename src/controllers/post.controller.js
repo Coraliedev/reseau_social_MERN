ea72c207a241568add62e18526329cc2cbef07f0
Coraliedev <coralie.acquaert@gmail.com>
2022-08-05 19:29:34 +0200

    feat(API): read post with GET method from /api/post

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -2,12 +2,12 @@ const PostModel = require("../models/post.model");
 const UserModel = require("../models/user.model");
 const ObjectID = require("mongoose").Types.ObjectId;
 
-// module.exports.readPost = (req, res) => {
-//   PostModel.find((err, docs) => {
-//     if (!err) res.send(docs);
-//     else console.log(`Error to get data ${err}`);
-//   });
-// };
+module.exports.readPost = (req, res) => {
+  PostModel.find((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log(`Error to get data ${err}`);
+  });
+};
 
 module.exports.createPost = async (req, res) => {
   const newPost = new PostModel({
