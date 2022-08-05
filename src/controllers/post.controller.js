b4a0ffae8ab6772d542632268bd8b507741053eb
Coraliedev <coralie.acquaert@gmail.com>
2022-08-05 21:30:09 +0200

    feat(API): add commentPost with PATCH methode from /api/post/comment-post/:id

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -83,29 +83,65 @@ module.exports.likePost = async (req, res) => {
 module.exports.unlikePost = async (req, res) => {
   if (!ObjectID.isValid(req.params.id))
     return res.status(400).send(`ID unknow ${req.params.id}`);
-    try {
-      PostModel.findByIdAndUpdate(
-        req.params.id,
-        {
-          $pull: { likers: req.body.id },
-        },
-        { new: true },
-        (err, docs) => {
-          if (err) return res.status(400).send(err);
-        }
-      );
-      UserModel.findByIdAndUpdate(
-        req.body.id,
-        {
-          $pull: { likes: req.params.id },
-        },
-        { new: true },
-        (err, docs) => {
-          if (!err) res.send(docs);
-          else res.status(400).send(err);
-        }
-      );
-    } catch (err) {
-      return res.status(400).send(err);
-    }
+  try {
+    PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: { likers: req.body.id },
+      },
+      { new: true },
+      (err, docs) => {
+        if (err) return res.status(400).send(err);
+      }
+    );
+    UserModel.findByIdAndUpdate(
+      req.body.id,
+      {
+        $pull: { likes: req.params.id },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) res.send(docs);
+        else res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 };
+
+module.exports.commentPost = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send(`ID unknow ${req.params.id}`);
+  try {
+    return PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          comments: {
+            commenterId: req.body.commenterId,
+            commenterPseudo: req.body.commenterPseudo,
+            text: req.body.text,
+            timestamp: new Date().getTime(),
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+// module.exports.editCommentPost = (req, res) => {
+//   if (!ObjectID.isValid(req.params.id))
+//     return res.status(400).send(`ID unknow ${req.params.id}`);
+// };
+// module.exports.deleteCommentPost = (req, res) => {
+//   if (!ObjectID.isValid(req.params.id))
+//     return res.status(400).send(`ID unknow ${req.params.id}`);
+// };
